7cfba6a971d0fea47164be08e313825fd9d2f29f
athiracev <athiracev@gmail.com>
2024-06-11 11:39:55 +0530

    ecart project designing completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { LoginComponent } from './login/login.component';
+import { RegComponent } from './reg/reg.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'detail/:id', component: DetailComponent },
+  { path: 'log', component: LoginComponent },
+  { path: 'reg', component: RegComponent },
+  { path: 'wish', component: WishlistComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'check', component: CheckoutComponent },
+  { path: '*', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { DetailComponent } from './detail/detail.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegComponent } from './reg/reg.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CheckoutComponent,
+    DetailComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    RegComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule,
